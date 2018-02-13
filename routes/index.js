04c8498d87fbeb9d667c511768dabb1b375fc688
Jake Martin Jacob Madsen <jake@iceshieldgaming.com>
2018-02-13 11:14:50 +0100

    sql db connection

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const log = require("../services/logging_module/logging_module");
+const sql = require("../config/sql.js").connect();
 module.exports = (app)=>{
     app.get("/",(req,res)=>{
         res.render("pages/index")
         log.activityLog(req.connection.remoteAddress + " /index");
+    })
+
+    sql.query("SELECT * FROM `tb_test`", function (err, data) {
+        console.log(data);
     })
 }
