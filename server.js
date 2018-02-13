c5e80b8eaafc2babdbff70c23e79dbd8ce2b413f
Timm Jensen <4342842@rts-365.dk>
2018-02-13 13:14:01 +0100

    Minor typos

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("port", process.env.PORT || 3000);
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+// Import routes
 require("./routes/routeIndex")(app);
+
 app.listen(app.get('port'), function () {
 	console.log('Listening on port ' + app.get('port'));
 });
