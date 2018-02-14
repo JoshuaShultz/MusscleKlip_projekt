0b099a9412cad67078ff1baa614eae3d85b0df17
Yunus Yesildal <4113779@RTS-365.dk>
2018-02-14 13:47:51 +0100

    Opret produkt CSS

--- public/js/produkter.js
+++ public/js/produkter.js
@@ -1,16 +1,27 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    let deleteprodukt = document.querySelectorAll(".delete");
-    deleteprodukt.forEach((element) => {
-        element.addEventListener("click", (event) => {
-            event.preventDefault();
-            fetch("http://localhost:3000/produkter/" + element.dataset.id, {
-                'method': 'delete',
-                'headers': {
-                    'Content-Type': 'application/json'
-                },
-                'mode': 'cors',
-                'cache': 'default'
+            let deleteprodukt = document.querySelectorAll(".delete");
+            deleteprodukt.forEach((element) => {
+                element.addEventListener("click", (event) => {
+                    event.preventDefault();
+                    fetch("http://localhost:3000/produkter/" + element.dataset.id, {
+                        'method': 'delete',
+                        'headers': {
+                            'Content-Type': 'application/json'
+                        },
+                        'mode': 'cors',
+                        'cache': 'default'
+                    })
+                })
             })
-        })
-    })
-})
+
+            let redigerprodukt = document.querySelectorAll(".edit");
+            redigerprodukt.forEach((element) => {
+                element.addEventListener("click", (event) => {
+                    event.preventDefault();
+                    console.log(element);
+                    fetch("http://localhost:3000/produkter/" + element.dataset.id)
+                    .then(data => data.())
+                    .then(data => console.log(data))
+                 })
+                })
+            });
