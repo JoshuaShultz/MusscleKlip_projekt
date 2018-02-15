255d5692edb912edd2c16400d4785afa6013af2e
MantasAkilanas <4294018@rts-365.dk>
2018-02-15 11:51:50 +0100

    extra stuff to index

--- routes/home.js
+++ routes/home.js
@@ -3,11 +3,13 @@ const sql_connection = require("../config/sql.js").connect();
 const json_export = require("../services/json_exporter_module/json_export_module.js")
 const log_module = require("../services/logging_module/logging_module.js");
 
-module.exports = (server)=>{
-    server.get("/",(req,res)=>{
-        res.render("pages/index",{
-            galleri:json_export.galleri(),
-            footer: json_export.footer()
+module.exports = (server) => {
+    server.get("/", (req, res) => {
+        res.render("pages/index", {
+            galleri: json_export.galleri(),
+            footer: json_export.footer(),
+            prices: json_export.prices(),
+            openHours: json_export.openHours()
         })
         log_module.activityLog(req.connection.remoteAddress + " /index");
     })
